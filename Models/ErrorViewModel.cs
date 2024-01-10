de899918e61244f221d48cd11ec2531cff344c90
tmnpierre <tomyn.pierre@gmail.com>
2024-01-10 22:34:02 +0100

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BlogSphere.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
